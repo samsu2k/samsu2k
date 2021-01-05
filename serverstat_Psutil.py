8d741dbf6b8d3ddce34a531dc26d171f44844457
samsu2k <77011951+samsu2k@users.noreply.github.com>
2021-01-05 16:18:15 +0100

    Update serverstat_Psutil.py

--- serverstat_Psutil.py
+++ serverstat_Psutil.py
@@ -3,6 +3,6 @@ import psutil as PSUTIL
 import time
 while True:
 
-    with open('/home/samwel/Desktop/SFNAssignment/venv/statistics5.log', "a") as myfile:
+    with open('yourfilelocation', "a") as myfile:
         myfile.write(str(PSUTIL.cpu_percent(interval=1)) + os.linesep)
     time.sleep(60)
